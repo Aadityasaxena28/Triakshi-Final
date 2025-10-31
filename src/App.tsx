467eea34741ad1f9180865c64422638e140b4ff1
Aadityasaxena28 <adityasaxena998@gmail.com>
2025-11-01 03:36:19 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ import RudrakshPage from "./components/rudraksha-category";
 import TermsConditions from "./components/Terms_conditions";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-
+import shipping-policy from "./components/shipping-policy";
 
 const queryClient = new QueryClient();
 
