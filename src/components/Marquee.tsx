c4270a892bb8925510c8e8c44ea9e3d431ee4cc4
Aadityasaxena28 <adityasaxena998@gmail.com>
2025-10-31 06:22:04 +0530

    spellings mistakes fixed

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -3,16 +3,16 @@ const Marquee = () => {
     <div className="bg-gradient-to-r from-primary to-gold-light py-3 overflow-hidden">
       <div className="animate-marquee whitespace-nowrap">
         <span className="text-white font-semibold text-lg mx-8">
-          JAI MAA PITAMBRA, WELCOME TO TRIYAKSHI GEMS, HAPPY SHOPPING
+          JAI MAA PITAMBARA, WELCOME TO TRIAKSHI GEMS, HAPPY SHOPPING
         </span>
         <span className="text-white font-semibold text-lg mx-8">
-          JAI MAA PITAMBRA, WELCOME TO TRIYAKSHI GEMS, HAPPY SHOPPING
+          JAI MAA PITAMBARA, WELCOME TO TRIAKSHI GEMS, HAPPY SHOPPING
         </span>
         <span className="text-white font-semibold text-lg mx-8">
-          JAI MAA PITAMBRA, WELCOME TO TRIYAKSHI GEMS, HAPPY SHOPPING
+          JAI MAA PITAMBARA, WELCOME TO TRIAKSHI GEMS, HAPPY SHOPPING
         </span>
         <span className="text-white font-semibold text-lg mx-8">
-          JAI MAA PITAMBRA, WELCOME TO TRIYAKSHI GEMS, HAPPY SHOPPING
+          JAI MAA PITAMBARA, WELCOME TO TRIAKSHI GEMS, HAPPY SHOPPING
         </span>
       </div>
     </div>
