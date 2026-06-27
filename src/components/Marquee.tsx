48ba1b33d07e59ba932b568772400b2a884242a1
Aadityasaxena28 <adityasaxena998@gmail.com>
2026-06-28 02:38:02 +0530

    Update Marquee.tsx

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -1,15 +1,15 @@
 const Marquee = () => {
   return (
-    <div className="bg-yellow-400 py-3 overflow-hidden">
+    <div className="bg-black py-3 overflow-hidden">
       <div className="animate-marquee whitespace-nowrap">
-        <span className="text-red-900 font-bold text-lg mx-8">
+        <span className="text-yellow-400 font-bold text-lg mx-8">
           JAI MAA PITAMBARA, WELCOME TO TRIAKSHI GEMS, HAPPY SHOPPING
         </span>
-        <span className="text-red-900 font-bold text-lg mx-8">
+        <span className="text-yellow-400 font-bold text-lg mx-8">
           The Third Eye Awakens.
         </span>
-        <span className="text-red-900 font-bold text-lg mx-8">
-        Triakshi Gems  provides 🔱 Authentic 🔱 Certified 🔱 Energized spiritual products.
+        <span className="text-yellow-400 font-bold text-lg mx-8">
+          Triakshi Gems provides 🔱 Authentic 🔱 Certified 🔱 Energized spiritual products.
         </span>
       </div>
     </div>
